1bb3560f776d9666f2b7ac1854456b20fa8edff9
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-03 03:11:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
-def reverse_each_word(sentence1)
+def reverse_each_word(sentence)
   # split_string = sentence1.split("")
   # reversed = []
   # sentence1.size.times { reversed << split_string.pop}
   # reversed.join
   # reversed
 
-  sentence1.each do |sentence1|
-    puts "#{sentence1.reverse}"
-  end
+  sentence.split.collect { |word| word.reverse}.join(" ")
 end
-reverse_each_word(["hello"])
+
