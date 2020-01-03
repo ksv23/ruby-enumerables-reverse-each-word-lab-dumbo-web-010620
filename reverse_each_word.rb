d6b04daabcd662e22e0346329f5ea5f49c5d563b
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-03 03:14:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,9 +6,5 @@ def reverse_each_word(sentence)
   # reversed
 
   sentence.split.collect { |word| word.reverse}.join(" ")
+  
 end
-
-
-reverse_each_word(sentence1)
-
-puts reverse_each_word(sentence1)
