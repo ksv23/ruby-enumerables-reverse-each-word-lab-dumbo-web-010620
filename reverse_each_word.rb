963378bd6704cf51545d8771640facfc2cecc443
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-03 03:14:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(sentence)
-  # split_string = sentence1.split("")
-  # reversed = []
-  # sentence1.size.times { reversed << split_string.pop}
-  # reversed.join
-  # reversed
+  split_string = sentence1.split("")
+  reversed = []
+  sentence1.size.times { reversed << split_string.pop}
+  reversed.join
 
-  sentence.split.collect { |word| word.reverse}.join(" ")
-  
+
+  # sentence.split.collect { |word| word.reverse}.join(" ")
+
 end
