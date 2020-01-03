fad730df299e5e2d94210c8ceecab3a7ed6925cc
Kring Villanueva <ksvillanueva7@gmail.com>
2020-01-03 03:02:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(sentence1)
-  split_string = sentence1.split("")
-  reversed = []
-  sentence1.size.times { reversed << split_string.pop}
-  reversed.join
-  reversed
+  # split_string = sentence1.split("")
+  # reversed = []
+  # sentence1.size.times { reversed << split_string.pop}
+  # reversed.join
+  # reversed
+
+  array = []
+  sentence1.each do |s|
+    array << yield(s)
+
+  end
+  array
 end
